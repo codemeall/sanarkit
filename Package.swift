e475cfb721db5ea975d25115a866a8f79405e669
Abdul Aziz <aziz@marensolutions.com>
2024-08-29 16:16:56 +0530

    added stimulator framework

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .binaryTarget(
                 name: "SanarKit",
-                path: "./Sources/SanarKit.xcframework")
+                path: "./Sources/SanarKit.framework")
         ]
 )
